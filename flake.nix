3951259003302ea083af25c48cbb2a0d98077d9f
Alex Batisse <alexandre.batisse@hey.com>
2025-08-01 10:30:19 +0200

    Add shell flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Trivy scanner runner environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          podman
+          uv
+          python312Packages.nox
+        ];
+      };
+  };
+}
