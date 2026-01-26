deadcdd167c04d2f53946d60cad523a0f4a352e6
Alex Batisse <alex.batisse@canonical.com>
2026-01-26 17:21:37 +0100

    Add diff task

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
-import nox
+import json
 from pathlib import Path
+
+import nox
 
 nox.options.default_venv_backend = "uv"
 nox.options.reuse_venv = "yes"
@@ -8,7 +10,10 @@ nox.options.sessions = []
 
 @nox.session
 def scan(session: nox.Session) -> None:
-    """Run Trivy scan on folder."""
+    """Run Trivy scan on folder.
+
+    Create a xlsx report with medium/high/critical CVEs.
+    """
     match session.posargs:
         case [path_candidate, *_]:
             if not (path := Path(path_candidate).resolve()).exists():
@@ -32,7 +37,7 @@ def scan(session: nox.Session) -> None:
         "-f",
         "json",
         "--output",
-        "report.json",
+        f"report_{path.name}.json",
         external=True,
     )
     session.log("Parsing Trivy report")
@@ -40,8 +45,45 @@ def scan(session: nox.Session) -> None:
         "uv",
         "run",
         "convert.py",
-        "report.json",
+        f"report_{path.name}.json",
         env={
             "UV_PROJECT_ENVIRONMENT": session.virtualenv.location,
         },
     )
+
+
+@nox.session
+def diff(session: nox.Session) -> None:
+    """Compare two json reports to display resolved CVEs.
+
+    Usage:
+        nox -s diff -- run_before.json run_after.json
+    """
+    match session.posargs:
+        case [path_candidate1, path_candidate2, *_]:
+            if not (path1 := Path(path_candidate1).resolve()).exists():
+                session.log(f"Cannot find {path1}.")
+                return
+            if not (path2 := Path(path_candidate2).resolve()).exists():
+                session.log(f"Cannot find {path2}.")
+                return
+        case _:
+            session.log("You must pass two paths to continue.")
+            return
+
+    with path1.open("r", encoding="utf8") as f:
+        report1 = json.load(f)
+
+    with path2.open("r", encoding="utf8") as f:
+        report2 = json.load(f)
+
+    cves_before = {
+        vuln["VulnerabilityID"] for vuln in report1["Results"][0]["Vulnerabilities"]
+    }
+    cves_after = {
+        vuln["VulnerabilityID"] for vuln in report2["Results"][0]["Vulnerabilities"]
+    }
+
+    diff = cves_before - cves_after
+    print(f"{len(diff)} CVE(s) addressed:")
+    print(" ".join(sorted(diff)))
